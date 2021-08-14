8d5c884a422721c82bb259dc325978e607416366
EdissonArley <edisson_094@hotmail.com>
2021-08-14 10:39:08 -0500

    Proyecto

---
+++ material-dashboard-angular2-master/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { FormsComponent } from '../../forms/forms.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    
+  ],
+  declarations: [
+    DashboardComponent,
+    UserProfileComponent,
+    FormsComponent,
+    TableListComponent,
+    NotificationsComponent,
+  ]
+})
+
+export class AdminLayoutModule {}
---
+++ material-dashboard-angular2-master/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { FormsComponent } from '../../forms/forms.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'forms',          component: FormsComponent },
+    { path: 'table-list',     component: TableListComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+];
